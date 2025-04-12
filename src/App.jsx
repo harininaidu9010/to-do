59a5bdd9d39cb4b0534b82f98e0c14f1bdccc42d
harininaidu9010 <harininaidu9010@example.com>
2025-04-12 07:35:32 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,114 @@
 import React from 'react'
 import './App.css'
-import {useState} from 'react'
+import { useState } from 'react'
+import JSConfetti from 'js-confetti'
+import animation  from "js-confetti"
 
 function App() {
 
-  const[task,setTask] = useState(["Learn React"])
-  const[completed,setCompleted] = useState(["Learn React"])
+  const[task, setTask] = useState([])
+  const[completed, setCompleted] = useState([])
+  const[text, setText] = useState()
+  const[cb, setCb] = useState(false)
 
- return(
-   <>
-   <div className='container'>
-     <h1 id='heading'>TO DO APP</h1>
+  const deleteTask = (place) =>{
+      task.splice(place,1)
+      setTask([...task])
+  }
 
-     <div className='child1'>
-       <input type='text' id='inp-box'/>
-       <button id='btn'>Add Task</button>
-     </div>
+  const completeTheTask = (place) =>{
+    const ani = new animation() 
+    ani.addConfetti({
+      emojis: ['🌈', '⚡️', '💥', '✨', '💫', '🌸'],
+   })
+    setTimeout(()=>{
+      let t = task.splice(place,1)
+      setCompleted([...completed,t])
+      setCb(false)
+      setTask([...task])
 
-     <div className='child2'>
+  },1000)
+}
 
-       <div className='ongoing'>
-        <h1>Ongoing Task</h1>
-           {task.map((item)=><li>{item}</li>)}  
-       </div>
+const undoTask = (place)=>{
+  setTimeout(()=>{
+    let t=completed.splice(place,1)
+    setTask([...task,t])
+    setCb(false)
+    setCompleted([...completed])
+  },500)
+}
 
-      
-        <div className='completed'>
-          <h1>completed task</h1>
-          {completed.map((item)=><li>{item}</li>)}
 
+  return (
+    <>
+        <div className='container'>
+          <div>
+              <h1 id='heading'>To Do App..</h1>
+          </div>
+            
+
+            <div className='child1'>
+              <input 
+              type='text' 
+              id='inp-box'
+              value={text}
+              onChange={(event)=>{setText(event.target.value)}}
+              />
+
+              <button id='btn' onClick={()=>{
+                let t = text.trim()
+                if(t==''){
+                  alert("Cant't add empty task")
+                }
+                else{
+                  setTask([...task,t])
+                }
+                
+                setText('')
+              }}>Add Task</button>
+            </div>
+
+            <div className='child2'>
+              <div className='ongoing'>
+                  <h1>Ongoing Task</h1>
+
+                  {task.map((item,index)=><div className='rendered-tasks'>
+
+                    <input type='checkbox'
+                    checked={cb}
+                    onChange={()=>{completeTheTask(index)}}
+                    />
+
+                    <li key={index}>{item}</li>
+                    <img src="/edit.png" 
+                    onClick={()=>{
+                      let upated_value = prompt("Previously: "+task[index])
+                      task.splice(index,1,upated_value)
+                      setTask([...task])
+                    }}
+                    />
+                    <img id={index} src="./delete.png" width="50" 
+                    onClick={()=>{deleteTask(index)}}/>
+                    
+                    </div>)}
+              </div>
+
+              <div className='completed'>
+                  <h1>Completed Task</h1>
+                  
+
+                  {completed.map((item, index)=>
+                   <div className='undo'> 
+                     <button onClick={()=>{undoTask(index)}}>undo</button>
+                     <li key={index}>{item}</li>
+                    </div> 
+                   )}
+              </div>
+            </div>
         </div>
-     </div>
-   </div>
-  </>
-)
-
+    </>
+  )
 }
 
 export default App
